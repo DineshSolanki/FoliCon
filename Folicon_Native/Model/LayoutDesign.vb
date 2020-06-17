6d183e4ea817c77f8fb2ecabcdf4e5bc284b6492
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2020-06-17 12:19:25 +0530

    fixed stackOverflow Exception in MovieIconLayout

--- Folicon_Native/Model/LayoutDesign.vb
+++ Folicon_Native/Model/LayoutDesign.vb
@@ -1,14 +1,15 @@
-﻿Imports FoliconNative.Modules
+﻿Imports System.Threading
+Imports FoliconNative.Modules
 Namespace Model
     Public Class LayoutDesign
-        Inherits MyMovieCIcon
+        Inherits MyMovieIconLayout
         Public Sub New()
-            MyBase.New(New MyMovieIconLayout(DummyFolderJpg.Result, "8.1", "visible", "visible"))
+            MyBase.New(DummyFolderJpg(), "8.1")
         End Sub
         Private Shared Async Function DummyFolderJpg() As Task(Of String)
-            Dim folderJpgPath = IO.Path.Combine(IO.Path.GetTempPath, "FoliconDummy.jpg")
+            Dim folderJpgPath = "H:\Users\ \Documents\Video\24 (IN)\24 (IN).jpg"
             If Not IO.File.Exists(folderJpgPath) Then
-                Await DownloadImageFromUrlAsync("https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_QL50_SY1000_CR0,0,674,1000_AL_.jpg", folderJpgPath)
+
             End If
             Return folderJpgPath
         End Function
