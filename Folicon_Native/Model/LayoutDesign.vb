f2cfc75414dcb8953793f2af833ed49fd496064e
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2020-07-02 21:28:33 +0530

    fixed error caused by design data bind mistake in recent last commit

--- Folicon_Native/Model/LayoutDesign.vb
+++ Folicon_Native/Model/LayoutDesign.vb
@@ -4,18 +4,14 @@ Namespace Model
     Public Class LayoutDesign
         Inherits MyMovieIconLayout
         Public Sub New()
-            MyBase.New("C:\dummy.jpg", "8.1", "visible", "visible")
+            MyBase.New(DummyFolderJpg(), "8.1")
         End Sub
-        Private Shared Async Function DummyFolderJpgAsync() As Task(Of String)
-            Dim tmpPath = IO.Path.GetTempPath
-            Dim folderJpgPath = IO.Path.Combine(tmpPath, "posterDummy.jpg")
-            'If Not IO.File.Exists(folderJpgPath) Then
-            '    Await DownloadImageFromUrlAsync("https://image.tmdb.org/t/p/original/r0bgHi3MwGHTKPWyJdORsb4ukY8.jpg", folderJpgPath)
-            'End If
+        Private Shared Async Function DummyFolderJpg() As Task(Of String)
+            Dim folderJpgPath = "H:\Users\ \Documents\Video\24 (IN)\24 (IN).jpg"
+            If Not IO.File.Exists(folderJpgPath) Then
+
+            End If
             Return folderJpgPath
-        End Function
-        Private Shared Async Function GetImageAsync(url As String, savePath As String) As Task
-            Await DownloadImageFromUrlAsync(url, savePath)
         End Function
     End Class
 End Namespace
