7ebdbf58243799685bb8845169e3171dc78ccbe8
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2021-08-04 19:47:06 +0530

    version bump to 3.5

--- FoliCon/ViewModels/AboutBoxViewModel.cs
+++ FoliCon/ViewModels/AboutBoxViewModel.cs
@@ -10,7 +10,7 @@ namespace FoliCon.ViewModels
 {
     public class AboutBoxViewModel : BindableBase, IDialogAware
     {
-        private string _title = "Folicon v3.4";
+        private string _title = "Folicon v3.5";
         private string _logo = "/Resources/folicon Icon.png";
 
         private string _description = LangProvider.GetLang("FoliConDescription");
