07e546021e72e3baf66b7d7908b2b9d81a0b7163
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2021-11-16 23:38:16 +0530

    made scrollbar visible again, changed hight of content list.

--- FoliCon/Views/HtmlBox.xaml.cs
+++ FoliCon/Views/HtmlBox.xaml.cs
@@ -37,7 +37,7 @@ namespace FoliCon.Views
         {
             if (!string.IsNullOrEmpty(HtmlText))
             {
-                var html = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta content='IE=Edge' http-equiv='X-UA-Compatible'><style>html{overflow:hidden;} .container {position: relative;overflow: hidden;padding-top: 56.25%; /* 16:9 Aspect Ratio */}.responsive-iframe {position: absolute;top: 0;left: 0;bottom: 0;right: 0;width: 100%;height: 100%;border: none;}</style></head><body><div class='container'> <iframe class='responsive-iframe' src='" + HtmlText + "' frameborder='0' allow =\"autoplay; fullscreen; clipboard - write; encrypted - media; picture-in-picture\" allowFullScreen></iframe></div></body></html>";
+                var html = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta content='IE=Edge' http-equiv='X-UA-Compatible'><style>.container {position: relative;overflow: hidden;padding-top: 56.25%; /* 16:9 Aspect Ratio */}.responsive-iframe {position: absolute;top: 0;left: 0;bottom: 0;right: 0;width: 100%;height: 100%;border: none;}</style></head><body><div class='container'> <iframe class='responsive-iframe' src='" + HtmlText + "' frameborder='0' allow =\"autoplay; fullscreen; clipboard - write; encrypted - media; picture-in-picture\" allowFullScreen></iframe></div></body></html>";
                 //string html = "<html><head>";
                 //html += "<meta content='IE=Edge' http-equiv='X-UA-Compatible'>" +
                 //    "<meta name='viewport' content='width = device - width, initial - scale = 1'>" +
