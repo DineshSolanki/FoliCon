3bbb87cefa7b93cff6fd04e333a9f11c0abe974b
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2022-03-22 23:29:32 +0530

    Fix trailerView scroll issue

--- FoliCon/Views/HtmlBox.xaml.cs
+++ FoliCon/Views/HtmlBox.xaml.cs
@@ -13,8 +13,8 @@
 
         public string HtmlText
         {
-            get { return (string)GetValue(HtmlTextProperty); }
-            set { SetValue(HtmlTextProperty, value); }
+            get => (string)GetValue(HtmlTextProperty);
+            set => SetValue(HtmlTextProperty, value);
         }
 
         protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
@@ -27,22 +27,39 @@
         }
         private void DoBrowse()
         {
-            if (string.IsNullOrEmpty(HtmlText)) return;
-            var html = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta content='IE=Edge' http-equiv='X-UA-Compatible'><style>.container {position: relative;overflow: hidden;padding-top: 56.25%; /* 16:9 Aspect Ratio */}.responsive-iframe {position: absolute;top: 0;left: 0;bottom: 0;right: 0;width: 100%;height: 100%;border: none;}</style></head><body><div class='container'> <iframe class='responsive-iframe' src='" + HtmlText + "' frameborder='0' allow =\"autoplay; fullscreen; clipboard - write; encrypted - media; picture-in-picture\" allowFullScreen></iframe></div></body></html>";
-            //string html = "<html><head>";
-            //html += "<meta content='IE=Edge' http-equiv='X-UA-Compatible'>" +
-            //    "<meta name='viewport' content='width = device - width, initial - scale = 1'>" +
-            //    "<style>.container {position: relative;width: 100 %;overflow: hidden;padding - top: 56.25 %;}" +
-            //    ".responsive - iframe {position: absolute;top: 0;left: 0;bottom: 0;right: 0;width: 100 %;height: 100 %;border: none;}</style>" +
-            //    "</head>" +
-            //    "<body>" +
-            //    "<div class='container'>";
+            if (string.IsNullOrEmpty(HtmlText) || HtmlText.Contains("Video not available!")) return;
+            //var html = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta content='IE=Edge' http-equiv='X-UA-Compatible'><style>.container {position: relative;overflow: hidden;padding-top: 56.25%; /* 16:9 Aspect Ratio */}.responsive-iframe {position: absolute;top: 0;left: 0;bottom: 0;right: 0;width: 100%;height: 100%;border: none;}</style></head><body><div class='container'> <iframe class='responsive-iframe' src='" + HtmlText + "' frameborder='0' allow =\"autoplay; fullscreen; clipboard - write; encrypted - media; picture-in-picture\" allowFullScreen></iframe></div></body></html>";
+            var html = $@"
+            <html>
+            <head>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<meta content='IE=Edge' http-equiv='X-UA-Compatible'>
+<style>html{{overflow:hidden;}}</style>
+<script src=""http://code.jquery.com/jquery-latest.min.js""
+        type=""text/javascript""></script>
+<script>$(function() {{
+  $('#video').css({{
+    width: $(window).innerWidth() + 'px',
+    height: $(window).innerHeight() + 'px'
+  }});
 
-            //html += $"<iframe class='responsive - iframe' id='video' src='{HtmlText}?rel=0' frameborder='0' allow =\"autoplay; fullscreen; clipboard - write; encrypted - media; picture-in-picture\" allowFullScreen></iframe>";
+  $(window).resize(function() {{
+    $('#video').css({{
+      width: $(window).innerWidth() + 'px',
+      height: $(window).innerHeight() + 'px'
+    }});
+  }});
+}});</script>
+                </ head>
+                <body>
+                    <iframe id='video' allow =autoplay; fullscreen; clipboard - write; encrypted - media; picture -in-picture' allowfullscreen='true'
+                        src ='{HtmlText}\' frameborder='0'>
+                    </iframe>
+                </body>
+            </html>";
 
-            //html += "</div></body></html>";
+
             browser.NavigateToString(html);
-            //browser.Navigate(HtmlText);
         }
     }
 }
