31f292ca27ef9c580125d29f96930ec2f6f72b61
Dinesh Solanki <15937452+dineshsolanki@users.noreply.github.com>
2022-12-03 00:19:52 +0530

    Remove system theme checkbox and merge with theme dropdown, change TextBlock to label for theme compatibility

---
+++ FoliCon/Models/FoliconThemes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoliCon.Models
+{
+    public enum FoliconThemes
+    {
+        //[LocalizedDescription("Light", typeof(Lang))]
+        Light,
+
+        //[LocalizedDescription("Dark", typeof(Lang))]
+        Dark,
+
+        //[LocalizedDescription("System", typeof(Lang))]
+        System
+    }
+}
