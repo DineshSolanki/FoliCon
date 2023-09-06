72105098293c3d2aa8f70c33c1cf77f16cd2fdc0
Dinesh Solanki <15937452+dineshsolanki@users.noreply.github.com>
2023-09-07 00:44:19 +0530

    commit missed file

---
+++ FoliCon/Modules/ReOrderDropHandler.cs
@@ -0,0 +1,25 @@
+﻿using GongSolutions.Wpf.DragDrop;
+
+namespace FoliCon.Modules;
+
+public class ReOrderDropHandler : IDropTarget
+{
+    private readonly DefaultDropHandler _defaultDropHandler = new();
+
+    public void DragOver(IDropInfo dropInfo)
+    {
+        if (dropInfo.DragInfo.VisualSource != dropInfo.VisualTarget)
+        {
+            dropInfo.Effects = DragDropEffects.None;
+            dropInfo.NotHandled = true;
+            return;
+        }
+
+        _defaultDropHandler.DragOver(dropInfo);
+    }
+
+    public void Drop(IDropInfo dropInfo)
+    {
+        _defaultDropHandler.Drop(dropInfo);
+    }
+}
