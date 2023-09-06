db035c1920588b73f6dbcfd13244368d94651920
Dinesh Solanki <15937452+dineshsolanki@users.noreply.github.com>
2023-09-06 22:18:14 +0530

    fixup! fix the issue of title bar not visible when using HandyWindow with prism dialog and make HandyWindow default DialogWindow

--- FoliCon/Modules/HandyWindow.cs
+++ FoliCon/Modules/HandyWindow.cs
@@ -2,11 +2,11 @@
 {
     public class HandyWindow : HandyControl.Controls.Window, IDialogWindow
     {
-        // static HandyWindow()
-        // {
-        //     DefaultStyleKeyProperty.OverrideMetadata(typeof(HandyWindow), new FrameworkPropertyMetadata(typeof(HandyControl.Controls.Window)));
-        //
-        // }
+        static HandyWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HandyWindow), new FrameworkPropertyMetadata(typeof(HandyControl.Controls.Window)));
+        
+        }
         public HandyWindow()
         {
             Background = (System.Windows.Media.Brush)FindResource("RegionBrush");
