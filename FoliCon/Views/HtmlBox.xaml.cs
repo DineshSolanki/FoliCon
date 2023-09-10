e629370c70d3edca92e7d6d0763cda910d5fd32b
Dinesh Solanki <15937452+dineshsolanki@users.noreply.github.com>
2023-09-10 20:11:40 +0530

    wip commit refactor htmlBox

--- FoliCon/Views/HtmlBox.xaml.cs
+++ FoliCon/Views/HtmlBox.xaml.cs
@@ -1,141 +1,122 @@
 ﻿using System.Windows.Navigation;
 using HandyControl.Themes;
 
-namespace FoliCon.Views
+namespace FoliCon.Views;
+
+/// <summary>
+/// Interaction logic for HtmlBox.xaml
+/// </summary>
+public partial class HtmlBox : UserControl
 {
-    /// <summary>
-    /// Interaction logic for HtmlBox.xaml
-    /// </summary>
-    public partial class HtmlBox : System.Windows.Controls.UserControl
+    private const string VideoUnavailable = "Video not available!";
+    
+    private readonly string _imagePath;
+
+    public static readonly DependencyProperty HtmlTextProperty
+        = DependencyProperty.Register(
+            nameof(HtmlText), 
+            typeof(string), 
+            typeof(HtmlBox), 
+            new PropertyMetadata(default(string), OnHtmlTextPropertyChanged));
+
+    public bool IsVideoAvailable => !string.IsNullOrEmpty(HtmlText) && !HtmlText.Contains(VideoUnavailable);
+
+    public HtmlBox()
     {
-        private readonly string _imagePath = Util.GetResourcePath("video-unavailable.png");
+        _imagePath = Util.GetResourcePath("video-unavailable.png");
 
-        public HtmlBox()
+        InitializeComponent();
+
+        Browser.Visibility = Visibility.Collapsed;
+    }
+
+    public string HtmlText
+    {
+        get => (string)GetValue(HtmlTextProperty);
+        set => SetValue(HtmlTextProperty, value);
+    }
+    private void UpdateBrowserVisibility()
+    {
+        Browser.Visibility = IsVideoAvailable ? Visibility.Visible : Visibility.Collapsed;
+    }
+    
+    private void ProcessBrowse()
+    {
+        if (Browser is not {IsLoaded: true}) return;
+
+        if (!IsVideoAvailable)
         {
-            InitializeComponent();
-            Browser.Visibility = Visibility.Hidden;
-            var theme = ThemeManager.Current.ApplicationTheme;
-            // var htmlText = """
-            //            <style>html, body {
-            //                overflow: hidden;
-            //                margin: 0;
-            //                padding: 0;
-            //                width: 100%;
-            //                height: 100%;
-            //                box-sizing: border-box;
-            //                background-color: #FFFFFF;
-            //            }</style>
-            //            """;
-            //
-            // if (theme == ApplicationTheme.Dark)
-            // {
-            //     htmlText = """
-            //                <style>html, body {
-            //                    overflow: hidden;
-            //                    margin: 0;
-            //                    padding: 0;
-            //                    width: 100%;
-            //                    height: 100%;
-            //                    box-sizing: border-box;
-            //                    background-color: #202020;
-            //                }</style>
-            //                """;
-            // }
-            //
-            // browser.NavigateToString(htmlText);
+            return;
         }
+        var content = GenerateHtmlContent();
+        
+        Browser.NavigateToString(content);
+    }
 
-        public static readonly DependencyProperty HtmlTextProperty = DependencyProperty.Register("HtmlText", typeof(string), typeof(HtmlBox));
+    private string GenerateHtmlContent()
+    {
+        return $$"""
+                 
+                             <html>
+                                 <head>
+                                     <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                     <meta content='IE=Edge' http-equiv='X-UA-Compatible'>
+                                     <style>
+                                         html, body {
+                                             overflow: hidden;
+                                             margin: 0;
+                                             padding: 0;
+                                             width: 100%;
+                                             height: 100%;
+                                             box-sizing: border-box;
+                                         }
+                                     </style>
+                                     <script src='https://code.jquery.com/jquery-latest.min.js' type='text/javascript'></script>
+                                     <script>
+                                         $(function() {
+                                             $('#video').css({
+                                                 width: $(window).innerWidth() + 'px',
+                                                 height: $(window).innerHeight() + 'px'
+                                             });
+                                             $(window).resize(function() {
+                                                 $('#video').css({
+                                                     width: $(window).innerWidth() + 'px',
+                                                     height: $(window).innerHeight() + 'px'
+                                                 });
+                                             });
+                                         });
+                                     </script>
+                                 </head>
+                                 <body>
+                                     <iframe id='video' allow='autoplay; fullscreen; clipboard-write; encrypted-media; picture-in-picture' allowfullscreen='true'
+                                         src='{{HtmlText}}' frameborder='0'>
+                                     </iframe>
+                                 </body>
+                             </html>
+                 """;
+    }
 
-        public string HtmlText
-        {
-            get => (string)GetValue(HtmlTextProperty);
-            set => SetValue(HtmlTextProperty, value);
-        }
+    private string GenerateUnavailableContent()
+    {
+        return $"""
+                
+                            <html>
+                                <body style='margin:0; padding:0; height: 100%; width: 100%; overflow: hidden;'>
+                                    <img src='{_imagePath}' style='height: 100%; width: 100%; object-fit: fill;' />
+                                </body>
+                            </html>
+                """;
+    }
 
-        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
-        {
-            base.OnPropertyChanged(e);
-            if (e.Property == HtmlTextProperty)
-            {
-                DoBrowse();
-            }
-        }
-        private void DoBrowse()
-        {
-            if (CheckAccess())
-            {
-                BrowseInternal();
-            }
-            else
-            {
-                Dispatcher.Invoke(BrowseInternal);
-            }
-           
-        }
+    private static void OnHtmlTextPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+    {
+        var control = source as HtmlBox;
+        control?.ProcessBrowse();
+        control?.UpdateBrowserVisibility();
+    }
 
-        private void BrowseInternal()
-        {
-            if (Browser is not { IsLoaded: true }) return;
-            if (string.IsNullOrEmpty(HtmlText) || HtmlText.Contains("Video not available!"))
-            {
-                var unavailableVideoContent = $"""
-                                               
-                                                   <html>
-                                                       <body style='margin:0; padding:0; height: 100%; width: 100%; overflow: hidden;'>
-                                                           <img src='{_imagePath}' style='height: 100%; width: 100%; object-fit: fill;' />
-                                                       </body>
-                                                   </html>
-                                               """;
-                Browser.NavigateToString(unavailableVideoContent);
-                return;
-            }
-            //var html = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta content='IE=Edge' http-equiv='X-UA-Compatible'><style>.container {position: relative;overflow: hidden;padding-top: 56.25%; /* 16:9 Aspect Ratio */}.responsive-iframe {position: absolute;top: 0;left: 0;bottom: 0;right: 0;width: 100%;height: 100%;border: none;}</style></head><body><div class='container'> <iframe class='responsive-iframe' src='" + HtmlText + "' frameborder='0' allow =\"autoplay; fullscreen; clipboard - write; encrypted - media; picture-in-picture\" allowFullScreen></iframe></div></body></html>";
-            var html = $$"""
-                         
-                                     <html>
-                                     <head>
-                         <meta name='viewport' content='width=device-width, initial-scale=1'>
-                         <meta content='IE=Edge' http-equiv='X-UA-Compatible'>
-                         <style>html, body {
-                             overflow: hidden;
-                             margin: 0;
-                             padding: 0;
-                             width: 100%;
-                             height: 100%;
-                             box-sizing: border-box;
-                         }</style>
-                         <script src="https://code.jquery.com/jquery-latest.min.js"
-                                 type="text/javascript"></script>
-                         <script>$(function() {
-                           $('#video').css({
-                             width: $(window).innerWidth() + 'px',
-                             height: $(window).innerHeight() + 'px'
-                           });
-                         
-                           $(window).resize(function() {
-                             $('#video').css({
-                               width: $(window).innerWidth() + 'px',
-                               height: $(window).innerHeight() + 'px'
-                             });
-                           });
-                         });</script>
-                                         </ head>
-                                         <body>
-                                             <iframe id='video' allow =autoplay; fullscreen; clipboard - write; encrypted - media; picture -in-picture' allowfullscreen='true'
-                                                 src ='{{HtmlText}}?hl={{LangProvider.Culture.TwoLetterISOLanguageName}}' frameborder='0'>
-                                             </iframe>
-                                         </body>
-                                     </html>
-                         """;
-
-
-            Browser.NavigateToString(html);
-        }
-
-        private void Browser_LoadCompleted(object sender, NavigationEventArgs e)
-        {
-            Browser.Visibility = Visibility.Visible;
-        }
+    private void Browser_LoadCompleted(object sender, NavigationEventArgs e)
+    {
+        Browser.Visibility = Visibility.Visible;
     }
 }
