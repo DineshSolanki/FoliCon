cf99c858730a246de451394b56ab710ef67fe0a7
Dinesh Solanki <15937452+dineshsolanki@users.noreply.github.com>
2023-09-24 17:38:43 +0530

    Refactor IGDB service exception handling
    Refactored the Exception handling of IGDBService for increased clarity and reliability. Replaced Contract.Assert checks with throwing InvalidOperationExceptions if the service client isn't initialized. This provides a more direct and explicit control flow. Also, returned response directly from method instead of creating temporary variable for cleaner code. Some methods are converted to single-line return statements for compactness.

--- FoliCon/Modules/IGDB/IgdbService.cs
+++ FoliCon/Modules/IGDB/IgdbService.cs
@@ -14,45 +14,47 @@ public class IgdbService(ref IGDBClient serviceClient)
     public async Task<ResultResponse> SearchGameAsync(string query)
     {
         Logger.Debug("Searching IGDB for {Query}", query);
-        Contract.Assert(_serviceClient != null);
+        if (_serviceClient is null) throw new InvalidOperationException("Service Client is not initialized.");
+
         var r = await _serviceClient.QueryAsync<Game>(IGDBClient.Endpoints.Games,
             $"search \"{query}\"; fields artworks.image_id, name,first_release_date,total_rating,summary,cover.*;");
 
-        var response = new ResultResponse
+        return new ResultResponse
         {
             MediaType = MediaTypes.Game,
             Result = r
         };
-        return response;
     }
-    
-    public async Task<GameVideo[]> GetGameVideo(string id)
+
+    public Task<GameVideo[]> GetGameVideo(string id)
     {
         Logger.Debug("Searching IGDB video by title id: {Id}", id);
-        Contract.Assert(_serviceClient != null);
-        var r = await _serviceClient.QueryAsync<GameVideo>(IGDBClient.Endpoints.GameVideos,
+        if (_serviceClient is null) throw new InvalidOperationException("Service Client is not initialized.");
+
+        return _serviceClient.QueryAsync<GameVideo>(IGDBClient.Endpoints.GameVideos,
             $"fields game,name,video_id; where game = {id};");
-        return r;
     }
-    
+
     public async Task<ResultResponse> SearchGameByIdAsync(string id)
     {
         Logger.Debug("Searching IGDB for {Id}", id);
-        Contract.Assert(_serviceClient != null);
-        var r = await _serviceClient.QueryAsync<Game>(IGDBClient.Endpoints.Games,
+        if (_serviceClient is null) throw new InvalidOperationException("Service Client is not initialized.");
+
+        var result = await _serviceClient.QueryAsync<Game>(IGDBClient.Endpoints.Games,
             $"fields artworks.image_id, name,first_release_date,total_rating,summary,cover.*; where id = {id};");
-        var response = new ResultResponse
+
+        return new ResultResponse
         {
             MediaType = MediaTypes.Game,
-            Result = r
+            Result = result
         };
-        return response;
     }
-    
-    public async Task<Artwork[]> GetArtworksByGameIdAsync(string id)
+
+    public Task<Artwork[]> GetArtworksByGameIdAsync(string id)
     {
-        var r = await _serviceClient.QueryAsync<Artwork>(IGDBClient.Endpoints.Artworks,
+        if (_serviceClient is null) throw new InvalidOperationException("Service Client is not initialized.");
+
+        return _serviceClient.QueryAsync<Artwork>(IGDBClient.Endpoints.Artworks,
             $"fields image_id; where game = {id};");
-        return r;
     }
 }
