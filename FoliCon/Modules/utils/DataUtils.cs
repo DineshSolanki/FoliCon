a326c05fdc45dea17eb7bd67e89c9a7b94a7d196
SARANG MK <35254554+sarangsmk@users.noreply.github.com>
2023-10-25 00:05:00 +0530

    Issue #145 Rating shows decimal points more than 2, in list and on rating shield (#158)
    * Fix for #https://github.com/DineshSolanki/FoliCon/issues/145
    "Rating shows decimal points more than 2, in list and on rating shield #145"
    Updated BuildFolderIco helper to Format the rating string,
    Added helper FormatRatingString in DataUtils.cs
    
    * Revert "Fix for #https://github.com/DineshSolanki/FoliCon/issues/145"
    
    This reverts commit 46218593803b32aa0622caa6ef4cdc929b05403f.
    
    * Fix for https://github.com/DineshSolanki/FoliCon/issues/145
    "Rating shows decimal points more than 2, in list and on rating shield #145"
    Updated BuildFolderIco helper to Format the rating string,
    Added helper FormatRatingString in DataUtils.cs
    
    * Update .gitignore
    
    * Updated FixRating shows decimal points more than 2, in list and on rating shield #145 with Review comments
    
    Updated Prop ListItem to trail to fixed digit.
    Updated FormatRatingString to FormatRating
    
    * refactor DataUtil to remove unused imports, use structured logging
    
    * refactor IconUtils.cs to remove unused import, remove rating formatting as it will already be taken care at earlier step
    
    fixes #145 
    ---------
    
    Co-authored-by: Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>

--- FoliCon/Modules/utils/DataUtils.cs
+++ FoliCon/Modules/utils/DataUtils.cs
@@ -1,9 +1,29 @@
-﻿namespace FoliCon.Modules.utils;
+﻿using NLog;
+
+namespace FoliCon.Modules.utils;
 
 public static class DataUtils
 {
+    private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+    
     public static int GetResultCount(bool isPickedById, dynamic result, string searchMode)
     {
         return isPickedById ? result != null ? 1 : 0 : searchMode == "Game" ? result.Length : result.TotalResults;
+    }
+
+    /// <summary>
+    /// Internal helper to format the Rating string for the selected Item.
+    /// The decimal points of rating should be limited to 2 digits : https://github.com/DineshSolanki/FoliCon/issues/145
+    /// Eg: X.XX -> 7.15
+    /// </summary>
+    /// <param name="ratingInput">Rating value.</param>
+    /// <returns>Formatted Rating value.</returns>
+    public static string FormatRating(double ratingInput)
+    {
+        Logger.Debug("Start FormatRatingString() - Input received : {RatingInput}", ratingInput);
+        var decimalPart = ratingInput % 1;
+        var formattedRatingValue = decimalPart > 0 ? ratingInput.ToString("0.##") : ratingInput.ToString("0");
+        Logger.Debug("End FormatRatingString() - Formatted Rating : {FormattedRatingValue}", formattedRatingValue);
+        return formattedRatingValue;
     }
 }
