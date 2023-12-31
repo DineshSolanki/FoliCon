df27d87b1f44dedd72d0bef893d3671946b53772
Dinesh Solanki <15937452+dineshsolanki@users.noreply.github.com>
2023-12-31 22:27:16 +0530

    Add OverlayNameToBooleanConvertor and update posterIconConfig
    A new Converter, OverlayNameToBooleanConvertor, has been introduced to handle the binding of overlay names. On the posterIconConfig view, alterations include using the new Converter, changing RadioButton styles, binding commands to IconOverlayChangedCommand, and setting the Window Height to "Auto".

---
+++ FoliCon/Modules/Convertor/OverlayNameToBooleanConvertor.cs
@@ -0,0 +1,23 @@
+﻿namespace FoliCon.Modules.Convertor;
+
+public class OverlayNameToBooleanConvertor : IMultiValueConverter
+{
+
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        return values.Length > 1 
+               && values[0] != null 
+               && values[1] != null 
+               && values[0].Equals(values[1]);
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        if (value != null && (bool)value)
+        {
+            return [null, (string)parameter];
+        }
+
+        return null;
+    }
+}
