9caccafece104cca8888844a8c00336c0209c5ba
Dinesh Solanki <15937452+dineshsolanki@users.noreply.github.com>
2024-02-09 00:37:25 +0530

    refactor for maintenance and readability

--- FoliCon/Modules/utils/TitleCleaner.cs
+++ FoliCon/Modules/utils/TitleCleaner.cs
@@ -13,13 +13,21 @@ internal static class TitleCleaner
     };
     public static string Clean(string title)
     {
-        var normalizedTitle = title.Replace('-', ' ').Replace('_', ' ').Replace('.', ' ');
+        var normalizedTitle = NormalizeTitle(title);
+        normalizedTitle = RemoveReplaceUnicodeCharacters(normalizedTitle);
+        var cleanTitle = CleanTitle(normalizedTitle);
 
-        normalizedTitle = UnicodeToNonUnicode.Aggregate(normalizedTitle, (current, pair) => normalizedTitle.Replace(pair.Key, pair.Value));
-        
-        // Remove all remaining unicode characters
-        normalizedTitle = Regex.Replace(normalizedTitle, @"[^\u0000-\u007F]+", string.Empty);
-        
+        Logger.Debug("Cleaned title: {Clean}, Original title: {Title}", cleanTitle, title);
+        return cleanTitle;
+    }
+    
+    private static string NormalizeTitle(string title)
+    {
+        return title.Replace('-', ' ').Replace('_', ' ').Replace('.', ' ');
+    }
+    
+    private static string CleanTitle(string title)
+    {
         // \s* --Remove any whitespace which would be left at the end after this substitution
         // \(? --Remove optional bracket starting (720p)
         // (\d{4}) --Remove year from movie
@@ -27,12 +35,19 @@ internal static class TitleCleaner
         // (year|resolutions) find at least one main token to remove
         // p?i? \)? --Not needed. To emphasize removal of 1080i, closing bracket etc, but not needed due to the last part
         // .* --Remove all trailing information after having found year or resolution as junk usually follows
-        var cleanTitle = Regex.Replace(normalizedTitle, "\\s*\\(?((\\d{4})|(420)|(720)|(1080))p?i?\\)?.*", "", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        
+        var cleanTitle = Regex.Replace(title, "\\s*\\(?((\\d{4})|(420)|(720)|(1080))p?i?\\)?.*", "", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         cleanTitle = Regex.Replace(cleanTitle, @"\[.*\]", "", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         cleanTitle = Regex.Replace(cleanTitle, " {2,}", " ", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-        var clean = string.IsNullOrWhiteSpace(cleanTitle) ? normalizedTitle : cleanTitle;
+
+        return string.IsNullOrWhiteSpace(cleanTitle) ? title : cleanTitle;
+    }
+    
+    private static string RemoveReplaceUnicodeCharacters(string title)
+    {
+        title = UnicodeToNonUnicode.Aggregate(title, (current, pair) => current.Replace(pair.Key, pair.Value));
         
-        Logger.Debug("Cleaned title: {Clean}, Original title: {Title}", clean, title);
-        return clean;
+        // Remove other remaining unicode characters
+        return Regex.Replace(title, @"[^\u0000-\u007F]+", string.Empty);
     }
 }
