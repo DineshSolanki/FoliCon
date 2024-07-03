8eb3ead16d1c1f333fd7eb31042a39ba342138c6
Dinesh Solanki <15937452+dineshsolanki@users.noreply.github.com>
2024-07-04 00:32:43 +0530

    Add support for read-only patterns in the UI
    Updated the `Pattern` class to include an ` IsReadOnly ` property and modified the `AppConfig` and `Patterns.xaml` to handle this new attribute. This ensures default patterns remain read-only, so they cannot be removed or edited.

--- FoliCon/Models/Data/Pattern.cs
+++ FoliCon/Models/Data/Pattern.cs
@@ -1,7 +1,9 @@
 ﻿namespace FoliCon.Models.Data;
 
-public class Pattern(string regex, bool isEnabled)
+public class Pattern(string regex, bool isEnabled, bool isReadOnly = false)
 {
     public string Regex { get; set; } = regex;
     public bool IsEnabled { get; set; } = isEnabled;
+    
+    public bool IsReadOnly { get; set; } = isReadOnly;
 }
--- FoliCon/Modules/Configuration/AppConfig.cs
+++ FoliCon/Modules/Configuration/AppConfig.cs
@@ -18,6 +18,6 @@ public class AppConfig : GlobalDataHelper
     public bool SubfolderProcessingEnabled { get; set; }
     
     public ObservableCollection<Pattern> Patterns { get; set; } =
-        [new Pattern("^[0-9]{1,2}x[0-9]{1,2}", false), new Pattern("S[0-9]{1,2}E[0-9]", false),
-            new Pattern("Season [0-9]{1,2} Episode [0-9]{1,2}", false), new Pattern("\\S+", true)];
+        [new Pattern("^[0-9]{1,2}x[0-9]{1,2}", false, true), new Pattern("S[0-9]{1,2}E[0-9]", false, true),
+            new Pattern("Season [0-9]{1,2} Episode [0-9]{1,2}", false, true), new Pattern("\\S+", true, true)];
 }
