80a4eb6f8b0288c0cf002618166f6241a9cdc244
Dinesh Solanki <15937452+dineshsolanki@users.noreply.github.com>
2024-07-19 10:48:07 +0530

    Fix: Update ExtractionProgress.cs to use Bendable base to notify the UI of the value change, as the progress bar wouldn't update and format progress bar text

--- FoliCon/Models/Data/ExtractionProgress.cs
+++ FoliCon/Models/Data/ExtractionProgress.cs
@@ -1,9 +1,17 @@
 ﻿namespace FoliCon.Models.Data;
 
-public class ExtractionProgress(int current, int total)
+public class ExtractionProgress : BindableBase
 {
-    public int Current { get; set; } = current;
-    public int Total { get; } = total;
+    private int _current;
+    private int _total;
+    public int Current { get => _current; set => SetProperty(ref _current, value); }
+    public int Total { get => _total; set => SetProperty(ref _total, value); }
     
     public double Progress => 100 * ((double)Current / Total);
+
+    public ExtractionProgress(int current, int total)
+    {
+        Current = current;
+        Total = total;
+    }
 }
