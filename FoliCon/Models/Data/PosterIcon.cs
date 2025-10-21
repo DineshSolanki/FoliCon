DineshSolanki/FoliCon

commit b56bb975757465e192f2f4e3b9c6ab58a25d9e2f

fix: simplify property setters and rename memory stream variable

FoliCon/Models/Data/PosterIcon.cs
@@ -10,33 +10,28 @@ public class PosterIcon : BindableBase
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
     public ImageSource FolderJpg { get; set; }
 
-    private string _rating;
-    private string _mockupVisibility;
-    private string _ratingVisibility;
-    private string _mediaTitle;
-
     public string Rating
     {
-        get => _rating;
-        set => SetProperty(ref _rating, value);
+        get;
+        set => SetProperty(ref field, value);
     }
 
     public string RatingVisibility
     {
-        get => _ratingVisibility;
-        set => SetProperty(ref _ratingVisibility, value);
+        get;
+        set => SetProperty(ref field, value);
     }
 
     public string MockupVisibility
     {
-        get => _mockupVisibility;
-        set => SetProperty(ref _mockupVisibility, value);
+        get;
+        set => SetProperty(ref field, value);
     }
 
     public string MediaTitle
     {
-        get => _mediaTitle;
-        set => SetProperty(ref _mediaTitle, value);
+        get;
+        set => SetProperty(ref field, value);
     }
 
     public PosterIcon()
@@ -44,8 +39,8 @@ public class PosterIcon : BindableBase
         try
         {
             var filePath = FileUtils.GetResourcePath("posterDummy.png");
-            using var thisMemoryStream = new MemoryStream(File.ReadAllBytes(filePath));
-            FolderJpg = (ImageSource)new ImageSourceConverter().ConvertFrom(thisMemoryStream);
+            using var memoryStream = new MemoryStream(File.ReadAllBytes(filePath));
+            FolderJpg = (ImageSource)new ImageSourceConverter().ConvertFrom(memoryStream);
         }
         catch (Exception ex)
         {
@@ -64,8 +59,8 @@ public class PosterIcon : BindableBase
         RatingVisibility = ratingVisibility;
         Rating = rating;
         MockupVisibility = mockupVisibility;
-        var thisMemoryStream = new MemoryStream(File.ReadAllBytes(folderJpgPath));
-        FolderJpg = (ImageSource)new ImageSourceConverter().ConvertFrom(thisMemoryStream);
+        using var memoryStream = new MemoryStream(File.ReadAllBytes(folderJpgPath));
+        FolderJpg = (ImageSource)new ImageSourceConverter().ConvertFrom(memoryStream);
     }
 
     public PosterIcon(string folderJpgPath, string rating, string ratingVisibility, string mockupVisibility,

commit 890bbcb68382f002a67e02fe81c591a203efa30c

fix issue caused by b56bb975757465e192f2f4e3b9c6ab58a25d9e2f: Don't let the stream dispose in constructor as its still need in .RenderToBitmap() later.

FoliCon/Models/Data/PosterIcon.cs
@@ -5,9 +5,12 @@ using Size = System.Windows.Size;
 namespace FoliCon.Models.Data;
 
 [Localizable(false)]
-public class PosterIcon : BindableBase
+public class PosterIcon : BindableBase, IDisposable
 {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    private MemoryStream _memoryStream;
+
     public ImageSource FolderJpg { get; set; }
 
     public string Rating
@@ -39,8 +42,9 @@ public class PosterIcon : BindableBase
         try
         {
             var filePath = FileUtils.GetResourcePath("posterDummy.png");
-            using var memoryStream = new MemoryStream(File.ReadAllBytes(filePath));
-            FolderJpg = (ImageSource)new ImageSourceConverter().ConvertFrom(memoryStream);
+            var bytes = File.ReadAllBytes(filePath);
+            _memoryStream = new MemoryStream(bytes);
+            FolderJpg = (ImageSource)new ImageSourceConverter().ConvertFrom(_memoryStream);
         }
         catch (Exception ex)
         {
@@ -59,8 +63,9 @@ public class PosterIcon : BindableBase
         RatingVisibility = ratingVisibility;
         Rating = rating;
         MockupVisibility = mockupVisibility;
-        using var memoryStream = new MemoryStream(File.ReadAllBytes(folderJpgPath));
-        FolderJpg = (ImageSource)new ImageSourceConverter().ConvertFrom(memoryStream);
+        var bytes = File.ReadAllBytes(folderJpgPath);
+        _memoryStream = new MemoryStream(bytes);
+        FolderJpg = (ImageSource)new ImageSourceConverter().ConvertFrom(_memoryStream);
     }
 
     public PosterIcon(string folderJpgPath, string rating, string ratingVisibility, string mockupVisibility,
@@ -85,5 +90,11 @@ public class PosterIcon : BindableBase
             300, 450, 96, 96, PixelFormats.Pbgra32);
         renderTargetBitmap.Render(drawingVisual);
         return renderTargetBitmap;
+    }
+
+    public void Dispose()
+    {
+        _memoryStream?.Dispose();
+        _memoryStream = null;
     }
 }